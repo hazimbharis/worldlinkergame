aa95e7d435a7bfb794f0fb1d471fe707239aef7c
Zim <98758788+hazimbharis@users.noreply.github.com>
2024-01-12 21:41:44 +0000

    janky drag camera
    its kinda bad

--- camera.gd
+++ camera.gd
@@ -6,7 +6,6 @@ const ZOOM_INCREMENT: float = 0.1
 const ZOOM_RATE: float = 8.0
 
 var _target_zoom: float = 1.0
-var zoom = 1
 
 func zoom_in() -> void:
 	_target_zoom = max(_target_zoom - ZOOM_INCREMENT, MIN_ZOOM)
@@ -17,16 +16,23 @@ func zoom_out() -> void:
 	set_physics_process(true)
 
 func _physics_process(delta: float) -> void:
-	zoom = lerp(zoom, int(_target_zoom) * 1, ZOOM_RATE * delta)
-	set_physics_process(not is_equal_approx(zoom.x, _target_zoom))
+	#zoom = lerp(zoom, int(_target_zoom) * 1, ZOOM_RATE * delta)
+	#set_physics_process(not is_equal_approx(zoom.x, _target_zoom))
+	pass
 
-func _unhandled_input(event: InputEvent) -> void:
+func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		if event.button_mask == MOUSE_BUTTON_MASK_MIDDLE:
-			position -= event.relative * zoom
+			position.x -= event.relative.x #* zoom
+			position.z -= event.relative.y
+		if event.button_mask == MOUSE_BUTTON_MASK_RIGHT:
+			rotation.x -= event.relative.x #* zoom
+			rotation.z -= event.relative.y
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+				fov -= ZOOM_RATE
 				zoom_in()
 			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+				fov += ZOOM_RATE
 				zoom_out()
